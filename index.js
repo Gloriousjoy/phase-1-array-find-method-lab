6a08876c389c606cb7b8a2121825f4bdc2989c35
Gloriousjoy <gloriousfab99@gmail.com>
2022-08-25 18:29:45 +0300

    array method find

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    const win = (record) => record.result === "W";
+    const winning = record.find(win)
+    if (winning){
+        return winning.year
+    }else{
+        return undefined;
+    }
+}
